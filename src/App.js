c3779766e363c3a3832c5735b8e27037d30db591
YanaFicus <166223418+YanaFicus@users.noreply.github.com>
2024-05-21 01:18:47 +0300

    add files

---
+++ src/App.js
@@ -0,0 +1,117 @@
+import React from "react"
+// import Header from "./components/Header"
+
+class App extends React.Component {
+  render(){
+    
+    return (<div className="name">
+      
+      <div class="heada">
+        <div class="overlay" />
+          <div class="iconufu"/>
+          <div class="logo2"/>
+          <div class="logo3"/>
+          <div class="button6">Главная</div>
+          <div class="button6">Направления</div>
+          <div class="button6">О школе</div>
+          <div class="menu1"/>
+          <div class="dropdown">
+          <div class="menu1"/>
+              <div class="dropdown-content">
+                <a href="#">Направления</a>
+                <a href="#">О школе</a>
+                <a href="#">Расписание</a>
+                <a href="#">Подать заявку</a>
+                <a href="#">Преподаватели</a>
+                <a href="#">Интенсивы</a>
+              </div>
+            </div>      
+          </div>
+      <div class="photoback"/>
+      <div class="motivat">
+        Погрузимся в мир <br/> информационных <br/> технологий вместе!
+      </div>
+      <div class="motivat2">
+        <p>Возрастная категория 12+</p>
+      </div>
+      <div class="zapis">
+        <p>Записаться</p>
+      </div><div class="startpng"/>
+      <div class="starttxt">Посмотрите<br/>видео про учебу<br/>в нашей школе</div>
+      <div class="rectangle0">
+        <p>О Школе ПРО IT</p>
+        <div class="inforect0"></div>
+      </div>
+      <div class="rectangle">
+        <div class="circle"><div class="circletxt">9</div> <br/>направлений</div>
+        <div class="circle"><div class="circletxt">7</div> <br/>интенсивов</div>
+        <div class="circle"><div class="circletxt">1000</div><br/>учеников</div>
+        <div class="circle"><div class="circletxt">3</div><br/>года на рынке</div>
+      </div>
+      <div class="rectangle2">
+        <div class="textrect2">
+          <p>Ознакомьтесь с направлениями</p>
+        </div>
+        <div class="column-top">
+          <div class="column">
+            <div class="cube"><div class="book"/></div>
+            <div class="cube"><div class="m1"/></div>
+            <div class="cube"><div class="m2"/></div>
+          </div>
+          <div class="column">
+            <div class="cube">1</div>
+            <div class="cube">2</div>
+            <div class="cube">3</div>
+          </div>
+          <div class="column">
+            <div class="cube">4</div>
+            <div class="cube">5</div>
+            <div class="cube">6</div>
+        </div>
+      </div>
+      </div>
+      <div class="rectangle3">
+        <div class="rectangle3text">А ещё... у нас есть интенсивы!</div>
+        <div class="inrect3-1"></div>
+        <div class="inrect3-2"></div>
+        <div class="inrect3-3"></div>
+        <div class="cuberect3">
+          <div class="startrect3"></div>
+        </div>
+        <div class="shes1"/>
+        <div class="cuberect3-2">
+          <p>Узнайте о школе ПРО IT <br/> подробнее за 2 минуты</p>
+        </div>
+        <div class="cuberect3-3">
+          <div class="textrect3-3">Ваше Имя</div>
+        </div>
+        <div class="cuberect3-4">
+          <div class="textrect3-4">+7 (___)___-__-_</div>
+        </div>
+        <div class="cuberect3-5">Отправить заявку</div>
+      </div>
+      <div class="rectangle4">
+        <div class="textrect4">
+          <p>Отзывы о Школе ПРО IT</p>
+        </div>
+        <div class="contrect4">
+          <div class="cuberect4"/>
+          <div class="cuberect4"/>
+          <div class="cuberect4"/>
+        </div>
+      </div>
+      <div class="rectangle5">
+        <div class="textrect5"><p>Фотогалерея</p></div>
+        <div class="pngrect5"/>
+      </div>
+      <div class="rectangleend">
+        <div class="map"/>
+        <div class="endiconufu"/>
+        <div class="endlogo2"/>
+        <div class="endlogo3"/>
+      </div>
+    </div>)
+  }
+}
+
+export default App
---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+// import React from "react"
+// import logo from "./logo.png"
+
+// class Header extends React.Component {
+//   render(){
+//         return(
+//             <header className="header">
+//                 <img src={logo}/>
+//                 <h2>Главная Направление О школе</h2>
+//             </header>
+            
+//     )
+//   }
+// }
+
+// export default Header
